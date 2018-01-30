51e601803cf3685dd70d870e56496ea0ca4341cc
Zoe-Massier <zermassier@gmail.com>
2018-01-30 10:41:18 -0600

    1/30/18

--- app/src/main/java/com/example/a592506/soundapp/MainActivity.java
+++ app/src/main/java/com/example/a592506/soundapp/MainActivity.java
@@ -13,4 +13,14 @@ public class MainActivity extends AppCompatActivity {
         MediaPlayer sound1 = MediaPlayer.create(getApplicationContext(), R.raw.sound1);
         sound1.start();
     }
+
+    public void email(View view) {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"sjoppel@gmail.com"});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, value: "Sound App");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, value: "Listen to my sound");
+
+        startActivity(emailIntent);
+    }
 }
