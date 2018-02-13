4eee2ea2dc046b348b4c2f72a236b3b0cffa0c88
Zoe-Massier <zermassier@gmail.com>
2018-02-13 10:40:02 -0600

    2/13/18

--- app/src/main/java/com/example/a592506/soundapp/MainActivity.java
+++ app/src/main/java/com/example/a592506/soundapp/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+    public void sound (View view) {
         MediaPlayer sound1 = MediaPlayer.create(getApplicationContext(), R.raw.sound1);
         sound1.start();
     }
@@ -17,10 +20,16 @@ public class MainActivity extends AppCompatActivity {
     public void email(View view) {
         Intent emailIntent = new Intent(Intent.ACTION_SEND);
         emailIntent.setType("text/plain");
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"sjoppel@gmail.com"});
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"zermassier@gmail.com"});
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, value: "Sound App");
         emailIntent.putExtra(Intent.EXTRA_TEXT, value: "Listen to my sound");
 
         startActivity(emailIntent);
     }
+
+    public void activity2(View view) {
+        Intent myIntent = new Intent(this, Activity2.class);
+        startActivity(myIntent);
+    }
+
 }
